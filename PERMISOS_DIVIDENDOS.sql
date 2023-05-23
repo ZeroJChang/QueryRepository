14e81854ef6836ff4372fa03dcb73dd6bb0b0118
ZeroJChang <jdeleonchang@gmail.com>
2023-05-23 12:58:59 -0600

    Create PERMISOS_DIVIDENDOS.sql
    add query permisos dividendos

---
+++ PERMISOS_DIVIDENDOS.sql
@@ -0,0 +1,16 @@
+use pinulito_dividendos
+use pinulito_nomina
+
+select * from templeado where codEmpleado = 3370
+
+select * from tusuarioRol where codEmpleado = 3370
+
+select * from trol where idrolmenu = 19
+
+select * from trol
+
+select * from tmenurol where idrol = 19
+
+insert into tmenurol(idrol, idmenu, vigente, creadopor) values (19, 134, 1, 4403)
+
+select * from tMenu
